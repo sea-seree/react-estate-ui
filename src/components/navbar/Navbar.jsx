36299e820a9e04cbbdb8d31aa07071356f266f0a
sea-seree <theseree7@gmail.com>
2024-03-23 23:05:04 +0700

    make responsive and  Navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./navbar.scss";
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>SeaEstate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sing in</a>
+        <a href="/" className="register">
+          Sing up
+        </a>
+        <div className="menuIcon">
+          <img src="/menu.png" alt="" onClick={() => setOpen(!open)} />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sing in</a>
+          <a href="/">Sing up</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
